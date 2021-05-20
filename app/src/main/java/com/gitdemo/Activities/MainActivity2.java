2df1b8996c622f8b0525a906557bcd67f2bfcc46
ramsakhare <sakhareram05@gmail.com>
2021-05-20 18:21:51 +0530

    Toast message added

---
+++ app/src/main/java/com/gitdemo/Activities/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.gitdemo.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.gitdemo.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toast.makeText(this, "Master Branch", Toast.LENGTH_SHORT).show();
+    }
+}
